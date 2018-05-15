232942df4cf36ea9fa8f9bc802037518a4e61fe2
Nick King <nking@opentext.com>
2018-05-15 19:43:58 +0100

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,22 @@
+
+// System level settings
+system = {
+	base_url: 'http://nkproc16.otdemo.net',
+	ps_org: 'system',
+	ps_instance: 'defaultInst',
+	ps_domain: 'otdemo.net'
+};
+
+// Application wide settings
+application = {
+	title: 'Bulk Loader',
+	desc_long: 'This tool allows you to pre-create entities by uploading Excel files with the entity data.  A sample Excel file can be downloaded from the link at the bottom of the page.'
+};
+
+// Logging wide settings
+logging = {
+	level: 'DEBUG',
+};
+
+
+
